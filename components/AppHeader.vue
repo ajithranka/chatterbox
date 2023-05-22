af94f94e03319b48050867973e66af940a922bd2
Ajith Ranka <ajithranka@users.noreply.github.com>
2023-05-22 07:19:21 +0100

    Setup basic i18n support

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,7 +2,7 @@
   <header
     class="cb-flex cb-items-center cb-justify-between cb-border-b cb-border-grey-lighter cb-bg-primary cb-px-6 cb-py-4"
   >
-    <h1 class="cb-font-medium">Chatterbox</h1>
+    <h1 class="cb-font-medium">{{ $t('app.title') }}</h1>
     <span
       class="cb-flex cb-h-9 cb-w-9 cb-items-center cb-justify-center cb-rounded-full cb-bg-grey-lighter cb-text-grey-dark"
     >
--- components/ChatSidebar.vue
+++ components/ChatSidebar.vue
@@ -3,7 +3,7 @@
     class="cb-space-y-10 cb-border-b cb-border-r cb-border-grey-lighter cb-bg-primary cb-px-6 cb-py-4"
   >
     <div class="cb-space-y-4">
-      <h2 class="cb-font-medium">Channels</h2>
+      <h2 class="cb-font-medium">{{ $t('chat.channels') }}</h2>
       <ul class="cb-space-y-3 cb-text-secondary">
         <li>#channel-name</li>
         <li>#channel-name</li>
@@ -11,7 +11,7 @@
       </ul>
     </div>
     <div class="cb-space-y-4">
-      <h2 class="cb-font-medium">Direct messages</h2>
+      <h2 class="cb-font-medium">{{ $t('chat.directMessages') }}</h2>
       <ul class="cb-space-y-3 cb-text-secondary">
         <li>@person</li>
         <li>@person</li>
